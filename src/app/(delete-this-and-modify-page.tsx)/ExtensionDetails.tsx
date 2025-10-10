6b5c22340466ffc719185bc2d782d906f21c3671
Siddhartha Maity <siddharthamaity1993@gmail.com>
2025-10-10 14:34:17 +0530

    chore: update dependencies and devDependencies in package.json
    - Bump Next.js version from 15.5.2 to 15.5.4
    - Update React and ReactDOM to version 19.2.0
    - Upgrade ESLint and related packages to latest versions
    - Update TypeScript and its related types to latest versions
    - Update Tailwind CSS and PostCSS to latest versions
    
    refactor: enhance ExtensionDetails component
    
    - Add links to recommended extensions for better accessibility
    - Adjust grid layout in ExtensionDetails for improved responsiveness

--- src/app/(delete-this-and-modify-page.tsx)/ExtensionDetails.tsx
+++ src/app/(delete-this-and-modify-page.tsx)/ExtensionDetails.tsx
@@ -1,26 +1,24 @@
 import Link from 'next/link';
 
 const RECOMMENDED_EXTENSIONS: string[] = [
-    'PulkitGangwar.nextjs-snippets',
-    'formulahendry.auto-close-tag',
-    'aaron-bond.better-comments',
-    'mikestead.dotenv',
-    'EditorConfig.EditorConfig',
-    'dbaeumer.vscode-eslint',
-    'MikeBovenlander.formate',
-    'donjayamanne.githistory',
-    'wix.vscode-import-cost',
-    'sburg.vscode-javascript-booster',
-    'christian-kohler.npm-intellisense',
-    'esbenp.prettier-vscode',
-    'Gruntfuggly.todo-tree',
-    'ChakrounAnas.turbo-console-log',
-    'codeandstuff.package-json-upgrade',
-    'KnisterPeter.vscode-commitizen',
-    'yzhang.markdown-all-in-one',
-    'bradlc.vscode-tailwindcss',
-    'austenc.tailwind-docs',
-    'stivo.tailwind-fold'
+    'PulkitGangwar.nextjs-snippets', // ? https://marketplace.visualstudio.com/items?itemName=PulkitGangwar.nextjs-snippets
+    'formulahendry.auto-close-tag', // ? https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-close-tag
+    'aaron-bond.better-comments', // ? https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
+    'mikestead.dotenv', // ? https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig
+    'EditorConfig.EditorConfig', // ? https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig
+    'dbaeumer.vscode-eslint', // ? https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+    'MikeBovenlander.formate', // ? https://marketplace.visualstudio.com/items?itemName=MikeBovenlander.formate
+    'donjayamanne.githistory', // ? https://marketplace.visualstudio.com/items?itemName=donjayamanne.githistory
+    'wix.vscode-import-cost', // ? https://marketplace.visualstudio.com/items?itemName=wix.vscode-import-cost
+    'sburg.vscode-javascript-booster', // ? https://marketplace.visualstudio.com/items?itemName=sburg.vscode-javascript-booster
+    'christian-kohler.npm-intellisense', // ? https://marketplace.visualstudio.com/items?itemName=christian-kohler.npm-intellisense
+    'esbenp.prettier-vscode', // ? https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+    'codeandstuff.package-json-upgrade', // ? https://marketplace.visualstudio.com/items?itemName=codeandstuff.package-json-upgrade
+    'KnisterPeter.vscode-commitizen', // ? https://marketplace.visualstudio.com/items?itemName=KnisterPeter.vscode-commitizen
+    'yzhang.markdown-all-in-one', // ? https://marketplace.visualstudio.com/items?itemName=yzhang.markdown-all-in-one
+    'bradlc.vscode-tailwindcss', // ? https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+    'austenc.tailwind-docs', // ? https://marketplace.visualstudio.com/items?itemName=austenc.tailwind-docs
+    'stivo.tailwind-fold' // ? https://marketplace.visualstudio.com/items?itemName=stivo.tailwind-fold
 ];
 
 interface ExtensionStatistics {
@@ -82,7 +80,7 @@ const ExtensionDetails: React.FC = async () => {
     const extensionDetails = await Promise.all(RECOMMENDED_EXTENSIONS.map(fetchExtensionDetails));
 
     return (
-        <div className='mx-auto grid max-w-2xl grid-cols-6 gap-y-3 sm:grid-cols-10 sm:gap-y-6'>
+        <div className='mx-auto grid max-w-2xl grid-cols-6 gap-y-3 sm:grid-cols-9 sm:gap-y-6'>
             {extensionDetails.map((extension) => {
                 return (
                     <div key={extension.name} className='group relative inline-flex justify-center'>
