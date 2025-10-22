a2c5adbcef270fe7c074e4fc7257423e8f8d7b1d
Trace Panic <patrickscript@proton.me>
2025-10-22 16:22:14 +0300

    Remove HomePage rendering from Page component
    Remove HomePage component and return null instead.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import HomePage from '@/app/(delete-this-and-modify-page.tsx)/HomePage';
-
-/**
- * The main page component that renders the HomePage component.
- *
- * @returns {JSX.Element} The rendered HomePage component.
- */
 const Page = () => {
-    return <HomePage />;
+    return null;
 };
 
 export default Page;
